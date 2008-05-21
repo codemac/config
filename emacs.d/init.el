8b34b0e375068d96333b79e7996b7eb3d528b3e6
Jeff Mickey <jeff@archlinux.org>
2008-05-21 16:27:34 -0400

    Urxvt change and Blog functions
    Urxvt defaults were changed so that the buffer search was removed
    from Alt-s which is used by weechat.
    
    The blog functions were added to make editing my next blog entry
    easier
    
    Signed-off-by: Jeff Mickey <jeff@archlinux.org>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -253,6 +253,38 @@
 
 ;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; bloggin
+;; This should create a new file with the next numerical value
+;; and add some boilerplate
+
+(defun blog-insert-meta ()
+  (interactive)
+  (insert "[[!meta title=\"\"]]\n")
+  (insert "[[!tag ]]\n")
+  (insert "\n")
+)
+
+(defun blog-find-next ()
+  (interactive)
+  (let ((wiki-dir "~/www/wiki/blog/"))
+    (find-file 
+     (concat wiki-dir 
+	     (number-to-string (1+ (apply 'max (mapcar 'string-to-number 
+	     (mapcar '(lambda (a) (substring a 0 -5)) 
+					 (directory-files wiki-dir nil "[0-9]*\\.mdwn" t))))))
+	     ".mdwn")))
+)
+
+(defun blog-next ()
+  (interactive)
+  (blog-find-next)
+  (end-of-buffer)
+  (blog-insert-meta)
+)
+
+;;
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; org mode
 (require 'org)
 
