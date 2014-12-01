6d24f11273c1bc1525894582bc52ec453566767f
Jeff Mickey <jm@igneous.io>
2014-12-01 13:14:10 -0800

    Better update

---
+++ emacs.d/elpa/pandoc-mode-20141127.1003/pandoc-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "pandoc-mode" "20141127.1003" "Minor mode for interacting with Pandoc" 'nil :keywords
+  '("text" "pandoc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/popup-20141120.1742/popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("popup.el") (21628 46309 871320 125000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
