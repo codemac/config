d93aad9873b81a81999b9754c19943e378bc600f
Jeff Mickey <jm@igneous.io>
2014-12-14 21:39:23 -0800

    Emacs update

--- emacs.d/elpa/ess-20141114.1503/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20141114.1503" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- emacs.d/elpa/pandoc-mode-20141127.1003/pandoc-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; pandoc-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "pandoc-mode" "pandoc-mode.el" (21628 46310
-;;;;;;  425266 641000))
-;;; Generated autoloads from pandoc-mode.el
-
-(autoload 'pandoc-mode "pandoc-mode" "\
-Minor mode for interacting with Pandoc.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'conditionally-turn-on-pandoc "pandoc-mode" "\
-Turn on pandoc-mode if a pandoc settings file exists.
-This is for use in major mode hooks.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("pandoc-mode-pkg.el") (21628 46310 518008
-;;;;;;  394000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pandoc-mode-autoloads.el ends here
