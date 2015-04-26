4d75ed1ad786b3800fff021bf0b60b9925b6267f
Jeff Mickey <j@codemac.net>
2015-04-26 15:03:12 -0700

    add back notion

---
+++ notion/cfg_notion.lua
@@ -0,0 +1,24 @@
+-- look at /etc/notion for descriptions
+META="Mod4+"
+ALTMETA=""
+
+XTERM="xterm"
+
+ioncore.set{
+    dblclick_delay=250,
+    kbresize_delay=1500,
+    opaque_resize=false,
+    warp=true,
+    switchto=true,
+    frame_default_index='next',
+    unsqueeze=true,
+    screen_notify=true,
+    autosave_layout=true,
+    mousefocus="sloppy",
+    window_stacking_request="ignore",
+    focuslist_insert_delay=1000,
+    activity_notification_on_all_screens=false,
+    workspace_indicator_timeout=0,
+}
+
+dopath("cfg_defaults")
