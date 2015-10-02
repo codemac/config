97e9b5502a86db28f81370e7c98b258f1674600b
Jeff Mickey <j@codemac.net>
2015-10-02 12:18:32 -0700

    sync

--- emacs.d/elpa/elfeed-org-20150412.145/elfeed-org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "elfeed-org" "20150412.145" "Configure elfeed with one or more org-mode files" '((elfeed "1.1.1") (org "8.2.7") (dash "2.10.0") (s "1.9.0")) :url "https://github.com/remyhonig/elfeed-org" :keywords '("news"))
--- emacs.d/elpa/s-20150923.4/s-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "s" "20150923.4" "The long lost Emacs string manipulation library." 'nil :keywords '("strings"))
