6d696fec06a5b041e084ceea25ce1df20f77c205
Jeff Mickey <j@codemac.net>
2017-06-01 11:23:52 -0700

    elpa update

---
+++ emacs.d/elpa/async-1.9.2/async-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from async.el
+(define-package "async" "1.9.2" "Asynchronous processing in Emacs" 'nil :url "http://elpa.gnu.org/packages/async.html" :keywords '("async"))
--- emacs.d/elpa/deferred-0.5.0/deferred-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "deferred" "0.5.0" "Simple asynchronous functions for emacs lisp" '((emacs "24.3")) :url "https://github.com/kiwanami/emacs-deferred" :keywords '("deferred" "async"))
