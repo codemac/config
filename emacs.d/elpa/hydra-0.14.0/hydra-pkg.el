6b25acfd34f0f8cc7a46c195e983c596d703994f
Jeff Mickey <j@codemac.net>
2017-08-05 17:10:53 -0700

    elpa sync

---
+++ emacs.d/elpa/hydra-0.14.0/hydra-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from hydra.el
+(define-package "hydra" "0.14.0" "Make bindings that stick around." '((cl-lib "0.5")) :url "https://github.com/abo-abo/hydra" :keywords '("bindings"))
---
+++ emacs.d/elpa/org-projectile-1.0.0/org-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-projectile" "1.0.0" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0") (emacs "24") (s "1.9.0") (org-category-capture "0.0.0")) :commit "d72c56458bd3b0535fc97dccaa23bd85dead099f" :url "https://github.com/IvanMalison/org-projectile" :keywords '("org-mode" "projectile" "todo" "tools" "outlines"))
