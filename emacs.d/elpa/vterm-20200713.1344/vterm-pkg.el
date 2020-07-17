7364949a31b3b3bc2f67250b2bf86d6218f76eb2
Jeff Mickey <j@codemac.net>
2020-07-17 12:20:06 -0700

    Add vterm to elpa

---
+++ emacs.d/elpa/vterm-20200713.1344/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20200713.1344" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "f41849c2c9c1899f22d1c3d4f871ec47c82627ce" :keywords
+  '("terminals")
+  :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
