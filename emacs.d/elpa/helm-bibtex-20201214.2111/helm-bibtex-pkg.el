0f4793d4fe32f7be8638a0f958fce70da7d9ea19
Jeff Mickey <j@codemac.net>
2021-08-22 15:51:11 -0700

    list timers is in boot

--- emacs.d/elpa/helm-bibtex-20201214.2111/helm-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from helm-bibtex-20201214.2111/helm-bibtex.el  -*- no-byte-compile: t -*-
-(define-package "helm-bibtex" "20201214.2111" "A bibliography manager based on Helm" '((bibtex-completion "1.0.0") (helm "1.5.5") (cl-lib "0.5") (emacs "24.1")) :commit "1bb81d77e08296a50de7ebfe5cf5b0c715b7f3d6" :authors '(("Titus von der Malsburg" . "malsburg@posteo.de")) :maintainer '("Titus von der Malsburg" . "malsburg@posteo.de") :url "https://github.com/tmalsburg/helm-bibtex")
