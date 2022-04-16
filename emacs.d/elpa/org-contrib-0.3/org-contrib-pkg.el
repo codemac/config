35696d1534ea294257c45109ca452855ab6ed4a5
Jeffrey Mickey <jmickey@fb.com>
2022-04-16 13:44:43 -0700

    org updates

---
+++ emacs.d/elpa/org-contrib-0.3/org-contrib-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from org-contrib.el  -*- no-byte-compile: t -*-
+(define-package "org-contrib" "0.3" "Unmaintained add-ons for Org-mode" '((emacs "25.1") (org "9.4.6")) :authors '(("Bastien Guerry" . "bzg@gnu.org")) :maintainer '("Bastien Guerry" . "bzg@gnu.org") :keywords '("org") :url "https://git.sr.ht/~bzg/org-contrib")
